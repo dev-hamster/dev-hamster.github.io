48e0049dd0c94da9f8a9755fe47ff8e1f00a266d
dev-hamster <hamsterwave0925@gmail.com>
2024-12-23 18:07:22 +0900

    chore: 폰트 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -41,6 +41,26 @@ const config: GatsbyConfig = {
         extensions: ['md'],
       },
     },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Ubuntu`,
+            file: `https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap`,
+          },
+          {
+            name: `Noto Sans KR`,
+            file: `https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 };
 
