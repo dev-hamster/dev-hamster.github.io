51e93c5f4b96664ff23c76468c66b478935e30b0
dev-hamster <hamsterwave0925@gmail.com>
2024-12-31 13:02:36 +0900

    chore: 이미지 플러그인 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,31 +6,32 @@ const config: GatsbyConfig = {
     title: `blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
-    'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        name: 'images',
-        path: './src/images/',
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`, // 이미지 인라인 처리
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
-      __key: 'images',
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'pages',
-        path: './src/pages/',
+        name: 'images',
+        path: './src/images/', // 이미지를 저장
       },
-      __key: 'pages',
+      __key: 'images',
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -44,6 +45,7 @@ const config: GatsbyConfig = {
       options: {
         alias: {
           components: path.resolve(__dirname, 'src/components'),
+          utils: path.resolve(__dirname, 'src/utils'),
         },
         extensions: ['md'],
       },
