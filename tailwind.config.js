53a71c55949bf0f81eb58a403062a7d6fcab71c5
dev-hamster <hamsterwave0925@gmail.com>
2025-01-30 21:56:15 +0900

    chore: tailwind theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,34 @@ module.exports = {
     `./src/content/**/*.mdx`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        noto: ['Noto Sans KR', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        midnight: {
+          200: '#CBD5E1',
+          500: '#64748b',
+          900: '#334155',
+        },
+        blue: {
+          200: '#d4e8ff',
+          500: '#3a97ed',
+          900: '#3a6ced',
+        },
+        purple: {
+          200: '#fbfaff',
+          500: '#7a3bff',
+        },
+        gray: {
+          200: '#f2f2f2',
+          300: '#f1f5f9',
+          400: '#e2e8f0',
+        },
+        black: '#222222',
+      },
+    },
   },
   plugins: [],
 };
