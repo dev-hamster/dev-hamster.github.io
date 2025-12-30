9967047108284b483e9bf8ec86937737975c5a45
dev-hamster <123740296+dev-hamster@users.noreply.github.com>
2025-12-31 02:29:29 +0900

    wip: about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,13 @@ import { HeadFC } from 'gatsby';
 import React from 'react';
 
 const AboutPage = () => {
-  return <PageLayout>안녕하세요 about page입니다.</PageLayout>
+  return <PageLayout>
+<div className='h-[100px] flex flex-col items-center justify-center'>
+ <div className='text-2xl'>￿</div>
+ <div>준비중입니다.</div>
+</div>
+
+  </PageLayout>
 };
 
 export default AboutPage;
