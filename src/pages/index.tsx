a627d89146c5f617c5ebaeac502bacd411a2ae7c
dev-hamster <123740296+dev-hamster@users.noreply.github.com>
2026-01-11 02:22:03 +0900

    feat: optimize GraphQL query by filtering posts at the source

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,7 @@ type IndexPageProps = {
 };
 
 const IndexPage = ({ data }: IndexPageProps) => {
-  // TODO: 성능 최적화 - GraphQL 쿼리 레벨에서 필터링하도록 변경
-  // 현재는 모든 MDX 노드를 가져온 후 JavaScript에서 필터링하므로,
-  // MDX 파일이 많아질수록 불필요한 데이터를 전송하게 됨.
-  // GraphQL 스키마에 type 필드를 명시적으로 정의한 후 filter를 사용하면 더 효율적임.
-  const posts = data.allMdx.nodes.filter(
-    (node) => !node.frontmatter.type || node.frontmatter.type !== 'log'
-  );
+  const posts = data.allMdx.nodes;
 
   return (
     <PageLayout>
@@ -54,7 +48,10 @@ export const Head: HeadFC = () => <title>dev-hamster</title>;
 
 export const query = graphql`
   query {
-    allMdx(sort: { frontmatter: { date: DESC } }) {
+    allMdx(
+      sort: { frontmatter: { date: DESC } }
+      filter: { frontmatter: { type: { ne: "log" } } }
+    ) {
       nodes {
         id
         frontmatter {
